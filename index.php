8a0467320ed937b363075549a25e4edffd594362
az1m0vv <129671021+az1m0vv@users.noreply.github.com>
2026-09-21 22:12:41 +0300

    lab2

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
-<?php
-    echo "<h1>Hello World! Це моя перша PHP-програма.</h1>";
-    phpinfo();
+<?php 
+$appName = "Task Manager";
+$taskTitle = "Вивчити основи PHP";
+$taskTimeEstimate = 2;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1><?= $appName ?></h1>
+    </header> 
+    <main>
+        <ul>
+            <li><?= $taskTitle ?></li>
+            <li><?= $taskTimeEstimate ?></li>
+        </ul>
+    </main>
+</body>
+</html>
